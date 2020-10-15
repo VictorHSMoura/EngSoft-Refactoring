e6a2ca96fe8a2ce227e3cdd44ba38005ceb0f338
VictorHSMoura <victorhugosmoura@gmail.com>
2020-10-15 19:32:56 -0300

    Commit 18

--- src/Statement.java
+++ src/Statement.java
@@ -1,6 +1,21 @@
+import java.util.Enumeration;
+
 public abstract class Statement {
 
     public abstract String header(Customer aCustomer);
     public abstract String body(Rental each);
     public abstract String footer(Customer aCustomer);
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = this.header(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for each rental
+            result += this.body(each);
+        }
+        //add footer lines
+        result += this.footer(aCustomer);
+        return result;
+    }
 }
