c04fd3deead352b4e19a719a25836a6c09e8623e
VictorHSMoura <victorhugosmoura@gmail.com>
2020-10-15 19:30:21 -0300

    Commit 17

--- src/Statement.java
+++ src/Statement.java
@@ -1,2 +1,6 @@
-public class Statement {
+public abstract class Statement {
+
+    public abstract String header(Customer aCustomer);
+    public abstract String body(Rental each);
+    public abstract String footer(Customer aCustomer);
 }
--- src/TextStatement.java
+++ src/TextStatement.java
@@ -4,16 +4,29 @@ public class TextStatement extends Statement {
 
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-                "\n";
+        String result = this.header(aCustomer);
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
+            //show figures for each rental
+            result += this.body(each);
         }
         //add footer lines
-        result += "Amount owed is " +
+        result += this.footer(aCustomer);
+        return result;
+    }
+
+    public String header(Customer aCustomer) {
+        return "Rental Record for " + aCustomer.getName() +
+                "\n";
+    }
+
+    public String body(Rental each) {
+        return "\t" + each.getMovie().getTitle()+ "\t" +
+                String.valueOf(each.getCharge()) + "\n";
+    }
+
+    public String footer(Customer aCustomer) {
+        String result = "Amount owed is " +
                 String.valueOf(aCustomer.getTotalCharge()) + "\n";
         result += "You earned " +
                 String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
